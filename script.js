2e9b2cb392d6eba9fc32bbbbd2ae42f8bc3d6acd
jlmaoju <151607503+jlmaoju@users.noreply.github.com>
2023-12-11 01:19:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('queryForm').addEventListener('submit', function(e) {
     var apiKey = document.getElementById('api_key').value;
     var query = document.getElementById('query').value;
 
-    fetch('https://171h34v184.imdo.co:5000/query', {
+    fetch('https://171h34v184.imdo.co/query', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
